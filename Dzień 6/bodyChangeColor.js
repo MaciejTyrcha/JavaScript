bde01b42a1f4d8571bd5716d37839c740f1e4834
aertuss <maciej.tyrcha@o2.pl>
2018-12-04 11:30:57 +0100

    dzien 6

---
+++ Dzień 6/bodyChangeColor.js
@@ -0,0 +1,14 @@
+document.body.addEventListener("click", (event) => {
+    const x = event.clientX;
+    const y = event.clientY;
+
+    console.log(`${x}.${y}`);
+
+    if ((x % 2 == 0) && (y % 2 == 0)) {
+        document.body.style.backgroundColor = "red";
+    } else if ((x % 2 != 0) && (y % 2 != 0)) {
+        document.body.style.backgroundColor = "blue";
+    } else {
+        document.body.style.backgroundColor = "green";
+    }
+});
