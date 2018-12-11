9bca0ed9f3bf53ce7248026556e02d22148e03d6
aertuss <maciej.tyrcha@o2.pl>
2018-12-11 16:31:47 +0100

    parralax image in the circle with a text appears

--- ParralaxOnWeb/functions.js
+++ ParralaxOnWeb/functions.js
@@ -19,13 +19,27 @@ $(window).scroll(function () {
 
     //Landing elements
     if (windowScroll > $('.clothes-pics').offset().top - $(window).height() / 1.2) {
-        console.log("hi");
+        // console.log("hi");
 
         $('.figure').each(function (i) {
             setTimeout(function () {
                 $('.figure').eq(i).addClass('is-showing');
             }, 150 * (i + 1));
 
+        });
+    }
+
+    //Circle image
+    if (windowScroll > $('.large-window').offset().top - $(window).height() / 1.2) {
+        // console.log("circle window");
+        $('.large-window').css({
+            'background-position': `center ${windowScroll - $('.large-window').offset().top}px`,
+        });
+
+        let opacity = (windowScroll - $('.large-window').offset().top + 400) / (windowScroll / 5);
+
+        $('.window-tint').css({
+            'opacity': `${opacity}`,
         });
     }
 });
